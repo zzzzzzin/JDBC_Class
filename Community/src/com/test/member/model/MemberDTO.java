d4c1f04ea36cc07e8884b9feb6a8f7d532d1e6f7
zzzzzzin <jyj7314@naver.com>
2024-02-28 10:44:56 +0900

    2024/02/28

--- Community/src/com/test/member/model/MemberDTO.java
+++ Community/src/com/test/member/model/MemberDTO.java
@@ -1,23 +1,17 @@
 package com.test.member.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 //DTO, Data Transfer Object
 //- 계층간 데이터를 전달하는 용도
+
+@Data	//Getter + Setter + ToString
+
 public class MemberDTO {
 	
 	private String id;
 	private String pw;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPw() {
-		return pw;
-	}
-	public void setPw(String pw) {
-		this.pw = pw;
-	}
 	
 }
